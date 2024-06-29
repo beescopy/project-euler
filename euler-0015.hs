9820fc410a1ed7d4b6316e49f89e80fa0b99417f
beescopy <beescopy@protonmail.ch>
2024-06-29 17:26:25 +0200

    15 in progress

---
+++ euler-0015.hs
@@ -0,0 +1,45 @@
+binToDec :: [Int] -> Int -> Int -> Int
+binToDec bInput bExp bMemory
+    | null bInput = bMemory
+    | otherwise   = binToDec (init bInput) (succ bExp) (bMemory + ((last bInput) * 2^bExp))
+
+-- maximum == 2^21 - 1 == 2.097.151
+decToBin :: Int -> Int -> [Int] -> [Int]
+decToBin bInput bExp bMemory
+    | bExp      <= -1     = bMemory
+    | 2^bExp    <= bInput = decToBin (bInput - 2^bExp) (bExp - 1) (bMemory ++ [1])
+    | otherwise           = decToBin (bInput)          (bExp - 1) (bMemory ++ [0])
+
+countOnes :: [Int] -> Int -> Int
+countOnes cList cCounter
+    | null cList      = cCounter
+    | head cList == 1 = countOnes (tail cList) (succ cCounter)
+    | otherwise       = countOnes (tail cList) (cCounter)
+    
+
+countElements :: [a] -> Int -> Int
+countElements cList cCounter
+    | null cList = cCounter
+    | otherwise  = countElements (tail cList) (succ cCounter)
+
+getAllRoutes :: Int -> Int -> [[Int]] -> [[Int]]
+getAllRoutes gMaximum gCounter gListOfRoutes
+    | gCounter >= gMaximum       = gListOfRoutes
+    | countOnes gDTB 0 == 10 = getAllRoutes gMaximum (succ gCounter) (gListOfRoutes ++ [gDTB])
+    | otherwise                  = getAllRoutes gMaximum (succ gCounter) gListOfRoutes
+    where
+        gDTB = decToBin gCounter 20 []
+
+main :: IO()
+main = do
+    print (binToDec [1,1,0,0,0,1] 0 0)
+    print (binToDec [1,1,0,0] 0 0)
+    print (binToDec [1,0,1,0] 0 0)
+    print (binToDec [1,0,0,1] 0 0)
+    print (binToDec [0,1,1,0] 0 0)
+    print (binToDec [0,1,0,1] 0 0)
+    print (binToDec [0,0,1,1] 0 0)
+    print (decToBin 1048576 20 [])
+    print (countElements [] 0)
+    print (countElements [1,1,2] 0)
+    print (getAllRoutes 16 0 [])
